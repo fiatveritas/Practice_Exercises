b7687b08e7ab1d0ae9779926e8c70a0567fc8ecf
fiatveritas <jesse.gallegos@usc.edu>
2018-12-06 18:34:38 -0800

    added file

---
+++ merge_sort.py
@@ -0,0 +1,41 @@
+#!usr/bin/python3
+
+def shrink_list(given_list):
+
+	if len(given_list) == 0:
+		print("Length zero list.", given_list)
+		return given_list
+	if len(given_list) == 1:
+		print("Length one list.", given_list)
+		return given_list
+	if len(given_list) > 0:
+		print("Length:", len(given_list), "Reduced List:", given_list)
+		on_the_left = shrink_list(left_list(given_list))
+		on_the_right = shrink_list(right_list(given_list))
+
+def left_list(given_list):
+	if int(len(given_list) / 2) > 0:
+		return given_list[:int(len(given_list) / 2)]
+	else:
+		return []
+
+def right_list(given_list):
+	if int(len(given_list) / 2) > 0:
+		return given_list[int(len(given_list) / 2):]
+	else:
+		return []			
+
+if __name__ == '__main__':
+	dud_list = []
+	zero_list = [0]
+	numbers = [i for i in range(9, 0, -1)]
+	print("Original Input:", numbers)
+	#shrink_list(dud_list) #base case of zero elements
+	#shrink_list(zero_list)
+	shrink_list(numbers)
+	'''print("jesse")
+	print("################")
+	print("################")
+	print("#END OF PROGRAM#")
+	print("################")
+	print("################")'''
