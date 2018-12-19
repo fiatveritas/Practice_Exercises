6e84edd4c620405d79822ed1e4265d9dfbd42719
fiatveritas <jesse.gallegos@usc.edu>
2018-12-18 18:59:35 -0800

    feat: experimented with order function

--- merge_sort.py
+++ merge_sort.py
@@ -32,9 +32,10 @@ def right_list(given_list):
 		return []			
 
 def ascending_order(left_array, right_array):
+	ordered_array = []
 	import random
 
-	random.seed(a = 0)
+	random.seed(a = 2)
 
 	left_random = random.sample(range(0, 50), 10)
 	right_random = random.sample(range(0, 50), 10)
@@ -42,10 +43,40 @@ def ascending_order(left_array, right_array):
 	right_size = len(right_random)
 	print("Left Size:", left_size, left_random)
 	print("Right Size:", right_size, right_random)
-	return []
+	print("xxxxxxxxxxxxxxx")
+
+	i = 0
+	j = 0
+	while(i < left_size):
+		while(j < right_size):
+			print(i, left_random[i])
+			print (j, right_random[j])
+			if left_random[i] > right_random[j]:
+				print("right is smaller")
+				print("xxxxxxxxxxxxxxx")
+				ordered_array.append(right_random[j])
+				j += 1
+			elif left_random[i] < right_random[j]:
+				print("left is smaller")
+				print("xxxxxxxxxxxxxxx")
+				ordered_array.append(left_random[i])
+				i += 1
+			else:
+				print("same size")
+				print("xxxxxxxxxxxxxxx")
+				ordered_array.append(left_random[i])
+				ordered_array.append(right_random[j])
+				i += 1
+				j += 1
+		if i < left_size:
+			ordered_array.append(left_random[i])
+			print(i, left_random[i])
+			print("xxxxxxxxxxxxxxx")
+			i += 1
+	return ordered_array
 
 if __name__ == '__main__':
-	print(ascending_order([], []))
+	print(ascending_order([], []), len(ascending_order([], [])))
 	#dud_list = []
 	#zero_list = [0]
 	#numbers = [i for i in range(9, 0, -1)]
