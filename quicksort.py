6bf14db063fe3b8e7559adae5713e6c523db610a
fiatveritas <jesse.gallegos@usc.edu>
2018-12-27 18:46:51 -0800

    status: deleted code..start over

--- quicksort.py
+++ quicksort.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
-import math
 import random
 
 def quicksort(to_be_sorted):
+	print("================")
+	print(to_be_sorted)
 	if len(to_be_sorted) == 0:
 		return []
 	if len(to_be_sorted) == 1:
 		return to_be_sorted
 	if len(to_be_sorted) > 1:
-		holder = math.inf
-		i = 0
 		j = 0
 		upper_limit = len(to_be_sorted)
 
-		while j < upper_limit:
-			if to_be_sorted[j] < to_be_sorted[upper_limit]:
-				print("index:", j, "element:", to_be_sorted[j])
+		for i in to_be_sorted:
+			if i < to_be_sorted[upper_limit - 1]:
 				j += 1
-			elif to_be_sorted[j] > to_be_sorted [upper_limit]:
-				print("index:", j, "element:", to_be_sorted[j])
-				holder = to_be_sorted[j]
-				to_be_sorted[j] = to_be_sorted[i]
-				to_be_sorted[i] = holder
-				i += 1
-				j += 1
-			#holder
+		if j < upper_limit - 1:
+			to_be_sorted[j], to_be_sorted[upper_limit - 1] = swap(to_be_sorted[j], to_be_sorted[upper_limit - 1])
+			quicksort(to_be_sorted[:: j])
+			quicksort(to_be_sorted[j + 1::])
+	print("================")
 	return to_be_sorted
+
+
+def swap(element_1, element_2):
+	holder = element_1
+	element_1 = element_2
+	element_2 = holder
+	return element_1, element_2
+
 
 if __name__ == "__main__":
 
 	random.seed(a = 0)
 	test_array = random.sample(range(0, 100), 10)
 
-	print("random list", test_array)
+	print("random list:", test_array)
 
 	#null_array = []
 	#array_len_one = [0]
@@ -40,3 +43,4 @@ if __name__ == "__main__":
 	#print(quicksort(null_array))
 	#print(quicksort(array_len_one))
 	print("sorted list:", quicksort(test_array))
+	print("orginal:", test_array)
