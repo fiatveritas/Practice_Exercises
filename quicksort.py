16ede0bd701d098de3ab247bdfc788e5131c5593
fiatveritas <jesse.gallegos@usc.edu>
2018-12-29 18:30:24 -0800

    feat: completed quicksort

--- quicksort.py
+++ quicksort.py
@@ -3,23 +3,36 @@ import random
 
 def quicksort(to_be_sorted):
 	print("================")
-	print(to_be_sorted)
+	print("to be sorted", to_be_sorted)
 	if len(to_be_sorted) == 0:
 		return []
 	if len(to_be_sorted) == 1:
 		return to_be_sorted
 	if len(to_be_sorted) > 1:
+		i = 0
 		j = 0
 		upper_limit = len(to_be_sorted)
-
-		for i in to_be_sorted:
-			if i < to_be_sorted[upper_limit - 1]:
-				j += 1
-		if j < upper_limit - 1:
-			to_be_sorted[j], to_be_sorted[upper_limit - 1] = swap(to_be_sorted[j], to_be_sorted[upper_limit - 1])
-			quicksort(to_be_sorted[:: j])
-			quicksort(to_be_sorted[j + 1::])
-	print("================")
+	#print("================")
+	while j < upper_limit - 1:
+		#print("i", i)
+		#print("j", j)
+		#print("list at start of iteration:", to_be_sorted)
+		if i == j and to_be_sorted[j] < to_be_sorted[upper_limit -1]:
+			i += 1
+			j += 1
+		elif to_be_sorted[j] > to_be_sorted[upper_limit -1]:
+			j += 1
+		elif to_be_sorted[j] < to_be_sorted[upper_limit -1]: # [49, 97, 53, 5, 33, 65, 62, 51, 38, 61]
+			to_be_sorted[i], to_be_sorted[j] = swap(to_be_sorted[i], to_be_sorted[j])
+			i += 1
+			j += 1
+		#print("list after iteration:", to_be_sorted)
+		#print("================")
+	to_be_sorted[i], to_be_sorted[upper_limit -1] = swap(to_be_sorted[i], to_be_sorted[upper_limit -1])
+	#print("list before recursion:", to_be_sorted)
+	#print("================")
+	to_be_sorted[:i] = quicksort(to_be_sorted[:i])
+	to_be_sorted[i:] = quicksort(to_be_sorted[i:])
 	return to_be_sorted
 
 
@@ -44,3 +57,4 @@ if __name__ == "__main__":
 	#print(quicksort(array_len_one))
 	print("sorted list:", quicksort(test_array))
 	print("orginal:", test_array)
+	# [49, 97, 53, 5, 33, 65, 62, 51, 38, 61]
