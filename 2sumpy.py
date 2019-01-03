fedc81bf0f97eaf919bbeff687519ce84c261c76
fiatveritas <jesse.gallegos@usc.edu>
2019-01-02 19:40:21 -0800

    feat: file creation

---
+++ 2sumpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random #for randomizing lists
+
+def 2sum(list_of_numbers):
+	look_up_table = {}
+
+if __name__ == "__main__":
+
+	null_array = []
+	array_len_one = [0]
+	for i in range(10):
+		random.seed(a = i)
+		test_array = random.sample(range(0, 100), 10)
+		print("Random list:", test_array)
