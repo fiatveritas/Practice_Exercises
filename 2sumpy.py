1ec1eab4eacc30e8ee2a61b0b7a2fdd39db5b151
fiatveritas <jesse.gallegos@usc.edu>
2019-01-04 20:31:42 -0800

    feat:testing hashtable base cases

--- 2sumpy.py
+++ 2sumpy.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 import random #for randomizing lists
 
-def 2sum(list_of_numbers):
+def sum_2(list_of_numbers, sum):
 	look_up_table = {}
+
+	print(look_up_table)
+
+	for i in list_of_numbers:
+		if not look_up_table:
+			look_up_table[i] = i
+	if list_of_numbers[0] in look_up_table:
+		print("first element of list in hashtable.")
+	print(look_up_table)
+	return sum
+
 
 if __name__ == "__main__":
 
@@ -12,3 +23,4 @@ if __name__ == "__main__":
 		random.seed(a = i)
 		test_array = random.sample(range(0, 100), 10)
 		print("Random list:", test_array)
+		print(sum_2(test_array, 5))
