888e9b61f59da163e1535f070b0e1ec2d2142865
fiatveritas <jesse.gallegos@usc.edu>
2019-01-04 20:49:47 -0800

    feat: implemented and coded 2u\sum algorithm beta testing

--- 2sumpy.py
+++ 2sumpy.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 import random #for randomizing lists
 
-def sum_2(list_of_numbers, sum):
+def sum_2(list_of_numbers, total):
 	look_up_table = {}
+	counter = 0
 
-	print(look_up_table)
+	print("Initializing empty hashtable:", look_up_table)
 
 	for i in list_of_numbers:
 		if not look_up_table:
 			look_up_table[i] = i
-	if list_of_numbers[0] in look_up_table:
-		print("first element of list in hashtable.")
-	print(look_up_table)
-	return sum
+		if total - i not in look_up_table:
+			look_up_table[i] = i
+		if total - i in look_up_table:
+			print (i, "+", total - i, "=", total)
+			counter += 1
+	print("after populating:", look_up_table)
+	return counter
 
 
 if __name__ == "__main__":
@@ -20,7 +24,11 @@ if __name__ == "__main__":
 	null_array = []
 	array_len_one = [0]
 	for i in range(10):
+		print("================")
 		random.seed(a = i)
 		test_array = random.sample(range(0, 100), 10)
+		num_1, num_2 = random.sample(test_array, 2)
+		total = num_1 + num_2
 		print("Random list:", test_array)
-		print(sum_2(test_array, 5))
+		print(sum_2(test_array, total), "pair(s) adding to", total)
+	print("================")
