3b66af0c9c13cd72c27c024957d64b4edf5ae5b1
fiatveritas <jesse.gallegos@usc.edu>
2019-01-05 19:02:59 -0800

    feat: minor tweaking to comments

--- 2sumpy.py
+++ 2sumpy.py
@@ -32,3 +32,23 @@ if __name__ == "__main__":
 		print("Random list:", test_array)
 		print(sum_2(test_array, total), "pair(s) adding to", total)
 	print("================")
+
+	
+# Algorithms: Design and Analysis Part 1
+# Stanford University
+
+# Programming Question 6 - Part 1
+# Count number of 2 sum variations where x + y = t
+# t is any integer in interval [-10000, 10000]
+# Hash table utilization problem
+
+import urllib2
+
+
+f = open('2_Sum.txt', 'r')
+lines = f.readlines()
+f.close()
+lines = [int(line) for line in lines]
+
+for i in range(20):
+    print lines[i], type(lines[i])
