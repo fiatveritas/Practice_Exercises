259b8625aefbc6764c96e7570cfd59d0e4dafef9
fiatveritas <jesse.gallegos@usc.edu>
2019-01-14 13:14:48 -0800

    feat: created pair look up table to avoid double counting

--- 2sumpy.py
+++ 2sumpy.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python3
 import random #for randomizing lists
 
-def sum_2(list_of_numbers, total, look_up_table):
+def sum_2(list_of_numbers, total, look_up_table, pairs_found):
 	counter = 0
 	#print("Passed hashtable:", look_up_table)
 
 	for i in list_of_numbers:
 		if not look_up_table:
-			look_up_table[i] = {i: False}
+			look_up_table[i] = i
 		if total - i not in look_up_table:
-			look_up_table[i] = {i: False}
+			look_up_table[i] = i
 		if total - i in look_up_table:
-			if look_up_table[total - i][total - i] == True:
+			num_1, num_2 = create_pairs(total - i, i)
+			if (num_1, num_2) in pairs_found:
 				continue
-			elif look_up_table[total - i][total - i] == False:
-				print (i, "+", total - i, "=", total)
-				look_up_table[total - i][total - i] = True
-				look_up_table[i] = {i: True}
+			else:
+				pairs_found[(num_1, num_2)] = True
+				#print (i, "+", total - i, "=", total)
 				counter += 1
-
 	#print("after populating:", look_up_table)
-	return counter, look_up_table
+	return counter, look_up_table, pairs_found
+
+def create_pairs(num_1, num_2):
+	if num_1 < num_2:
+		return num_1, num_2
+	elif num_2 < num_1:
+		return num_2, num_1
+	else:
+		return num_1, num_2
 
 def check_algorithm():
 	"""this method generates a few randomized
 	   lists to test the 2sum algorithm."""
 	null_array = []
 	array_len_one = [0]
+
 	for i in range(10):
 		print("================")
 		random.seed(a = i)
 		look_up_table = {}
+		pairs_found = {}
 		test_array = random.sample(range(0, 100), 10)
 		num_1, num_2 = random.sample(test_array, 2)
 		total = num_1 + num_2
 		print("Random list:", test_array)
-		print(sum_2(test_array, total, look_up_table)[0], "pair(s) adding to", total)
+		print(sum_2(test_array, total, look_up_table, pairs_found)[0], "pair(s) adding to", total)
 
 def read_file():
 	f = open('2_Sum.txt', 'r')
@@ -50,23 +59,19 @@ def read_file():
 def run_2sum(list_of_numbers):
 	counter = 0
 	look_up_table = {}
-	for t in range(-10, 11):
-		print("sum:", t)
-		count, look_up_table = sum_2(list_of_numbers, t, look_up_table)
+	pairs_found = {}
+	for t in range(-10000, 10001):
+		#print("sum:", t)
+		count, look_up_table, pairs_found = sum_2(list_of_numbers, t, look_up_table, pairs_found)
 		counter += count
-		print("pairs:", counter)
+		#print("pairs:", counter)
 	#print(look_up_table)
 	return counter
 
 
 if __name__ == "__main__":
-	#list_of_numbers = read_file()
-	for i in range(10):
-		print("================")
-		random.seed(a = i)
-		test_array = random.sample(range(-10, 11), 10)
-		print(test_array)
-		print(run_2sum(test_array))
+	list_of_numbers = read_file()
+	print(run_2sum(list_of_numbers))
 	#check_algorithm()
 	print("================")
 
